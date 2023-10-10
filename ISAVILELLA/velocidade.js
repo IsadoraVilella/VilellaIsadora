7b338bb46db3aaa1b69cf27f1063e170f90cee4f
IsadoraVilella <104770766+IsadoraVilella@users.noreply.github.com>
2023-10-10 12:38:07 -0300

    Add files via upload

---
+++ ISAVILELLA/velocidade.js
@@ -0,0 +1,22 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//ouvinte
+
+frm = addEventListener("submit", (e) => {
+    e.preventDeFault()
+ 
+    const permitida = Number(frm.inPermitida.value)
+    const condutor = Number(frm.inCondutor.value)
+    alert(permitida + condutor)
+    let leve = permitida + (permitida * 0.2)
+    
+
+    if(condutor <= permitida){
+        resp.innerText = `Situação: Sem Multa`
+    }else if(condutor <= leve){
+        resp.innerText = `Situação: Multa Leve`
+    }else{
+        resp.innerText = `Sitação: Multa Grave`
+    }
+})
